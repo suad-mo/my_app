9c733c5e1483a401211dc9867fa20390c7afd6e4
Suad Krvavac <suad.krvavac@gmail.com>
2022-06-30 10:00:37 +0200

    Add Raised Button...

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,43 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  //const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Mi First App'),
         ),
-        body: Text('This defaault text!'),
+        body: Column(
+          children: [
+            Text('The questions!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                //...
+                print('Answer 3 chosen!');
+              },
+            )
+          ],
+        ),
       ),
     );
   }
